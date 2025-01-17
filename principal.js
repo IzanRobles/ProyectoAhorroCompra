2eec0f861b212b4feebfb529f22c2875fdd89de0
DESKTOP-GFHMF6F\Javier <jrapadodominguez@gmail.com>
2025-01-17 09:25:45 +0100

    TJP_SP1_17122024 loginFuncional + registroFuncional

---
+++ principal.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", () => {
+    // Obtener la ID del usuario de la URL
+    const params = new URLSearchParams(window.location.search);
+    const userId = params.get("id");
+
+    if (userId) {
+        console.log(`ID del usuario: ${userId}`);
+        // Aquí puedes usar la ID para realizar solicitudes o mostrar datos específicos del usuario
+    } else {
+        console.error("No se encontró la ID del usuario en la URL.");
+    }
+});
